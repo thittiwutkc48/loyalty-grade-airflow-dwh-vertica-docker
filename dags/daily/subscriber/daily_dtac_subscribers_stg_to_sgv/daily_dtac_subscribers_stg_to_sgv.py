fa877b1a0c464335a8a44a153512b65577ce47cf
thittiwutkc48 <thittiwut.n@gmail.com>
2025-03-12 17:16:30 +0700

    --commit--

--- dags/daily/subscriber/daily_dtac_subscribers_stg_to_sgv/daily_dtac_subscribers_stg_to_sgv.py
+++ dags/daily/subscriber/daily_dtac_subscribers_stg_to_sgv/daily_dtac_subscribers_stg_to_sgv.py
@@ -7,11 +7,10 @@ from daily.subscriber.daily_dtac_subscribers_stg_to_sgv.default_config import de
 from daily.subscriber.daily_dtac_subscribers_stg_to_sgv.sql.sql_query import (
     extract_to_staging,
     delete_duplicates_staging,
+    delete_duplicates_single_staging,
     transform_to_single_staging,
     transform_to_unmatched_single_staging,
-    transform_inactive_to_single_view,
-    insert_to_product_history,
-    transform_active_to_single_view
+    transform_to_single_view,
 )
 # from plugins.sengrid.sendgrid_send_email import success_callback, failure_callback
 
@@ -52,6 +51,12 @@ with DAG(
         sql=Template(extract_to_staging).render(sql_context),
     )
 
+    delete_duplicates_single_staging_task = VerticaOperator(
+        task_id="delete_duplicates_single_staging_task",
+        vertica_conn_id="vertica_conn",
+        sql=Template(delete_duplicates_single_staging).render(sql_context),
+    )
+
     transform_to_single_staging_task = VerticaOperator(
         task_id="transform_to_single_staging_task",
         vertica_conn_id="vertica_conn",
@@ -64,23 +69,11 @@ with DAG(
         sql=Template(transform_to_unmatched_single_staging).render(sql_context),
     )
 
-    transform_inactive_to_single_view_task = VerticaOperator(
-        task_id="transform_inactive_to_single_view_task",
+    transform_to_single_view_task = VerticaOperator(
+        task_id="transform_to_single_view_task",
         vertica_conn_id="vertica_conn",
-        sql=Template(transform_inactive_to_single_view).render(sql_context),
+        sql=Template(transform_to_single_view).render(sql_context),
     )
 
-    insert_to_product_history_task = VerticaOperator(
-        task_id="insert_to_product_history_task",
-        vertica_conn_id="vertica_conn",
-        sql=Template(insert_to_product_history).render(sql_context),
-    )
-
-    transform_active_to_single_view_task = VerticaOperator(
-        task_id="transform_active_to_single_view_task",
-        vertica_conn_id="vertica_conn",
-        sql=Template(transform_active_to_single_view).render(sql_context),
-    )
-
-    delete_duplicates_staging_task >> extract_to_staging_task >> transform_to_single_staging_task >> transform_to_unmatched_single_staging_task >> transform_inactive_to_single_view_task >> insert_to_product_history_task >> transform_active_to_single_view_task
+    delete_duplicates_staging_task >> extract_to_staging_task >> delete_duplicates_single_staging_task >>transform_to_single_staging_task >> transform_to_unmatched_single_staging_task >> transform_to_single_view_task
     
--- dags/daily/subscriber/daily_dtac_subscribers_stg_to_sgv/sql/sql_query.py
+++ dags/daily/subscriber/daily_dtac_subscribers_stg_to_sgv/sql/sql_query.py
@@ -89,7 +89,7 @@ WITH ranked_subscribers AS (
     SELECT 
         *,
         ROW_NUMBER() OVER (
-            PARTITION BY customer_no, product_id
+            PARTITION BY product_id
             ORDER BY last_chng_dttm DESC
         ) AS row_num
     FROM {{ grading_schema }}.{{ staging_table }}
@@ -160,7 +160,7 @@ WITH ranked_subscribers AS (
     SELECT 
         *,
         ROW_NUMBER() OVER (
-            PARTITION BY customer_no, product_id
+            PARTITION BY product_id
             ORDER BY last_chng_dttm DESC
         ) AS row_num
     FROM {{ grading_schema }}.{{ staging_table }}
@@ -224,17 +224,15 @@ LEFT JOIN {{ grading_schema }}.{{ single_view_customer_table }} sdc
 WHERE sdc.grading_account_id IS NULL;
 """
 
-transform_inactive_to_single_view = """
+transform_to_single_view = """
 MERGE INTO {{ grading_schema }}.{{ single_view_subscriber_table }} AS target
 USING (
     SELECT * 
     FROM {{ grading_schema }}.{{ single_staging_table }}
     WHERE partition_date = '{{ partition_date }}'
         AND operator_name = 'DTAC'
-        AND product_status = 'INACTIVE'
 ) AS source
 ON target.product_id = source.product_id
-AND target.customer_no = source.customer_no
 AND target.operator_name = source.operator_name
 WHEN MATCHED THEN 
     UPDATE SET 
@@ -242,6 +240,7 @@ WHEN MATCHED THEN
         identifier_no = source.identifier_no,
         id_type = source.id_type,
         customer_type = source.customer_type,
+        customer_no = source.customer_no,
         golden_id = source.golden_id,
         product_name = source.product_name,
         product_name_desc = source.product_name_desc,
@@ -375,166 +374,166 @@ WHEN NOT MATCHED THEN
     );
 """
 
-insert_to_product_history = """
-INSERT INTO {{ grading_schema }}.{{ single_view_history_table }}
-SELECT * FROM {{ grading_schema }}.{{ single_view_subscriber_table }}
-WHERE product_status = 'INACTIVE'
-    AND operator_name = 'DTAC';
-"""
+# insert_to_product_history = """
+# INSERT INTO {{ grading_schema }}.{{ single_view_history_table }}
+# SELECT * FROM {{ grading_schema }}.{{ single_view_subscriber_table }}
+# WHERE product_status = 'INACTIVE'
+#     AND operator_name = 'DTAC';
+# """
 
-delete_inactive_in_single_view = """
-DELETE FROM {{ grading_schema }}.{{ single_view_subscriber_table }}
-WHERE product_status = 'INACTIVE' 
-    AND operator_name = 'DTAC';
-"""
+# delete_inactive_in_single_view = """
+# DELETE FROM {{ grading_schema }}.{{ single_view_subscriber_table }}
+# WHERE product_status = 'INACTIVE' 
+#     AND operator_name = 'DTAC';
+# """
 
-transform_active_to_single_view = """
-MERGE INTO {{ grading_schema }}.{{ single_view_subscriber_table }} AS target
-USING (
-    SELECT * 
-    FROM {{ grading_schema }}.{{ single_staging_table }}
-    WHERE partition_date = '{{ partition_date }}'
-        AND operator_name = 'DTAC'
-        AND product_status = 'ACTIVE'
-) AS source
-ON target.product_id = source.product_id
-AND target.customer_no = source.customer_no
-AND target.operator_name = source.operator_name
-WHEN MATCHED THEN 
-    UPDATE SET 
-        grading_account_id = source.grading_account_id,
-        identifier_no = source.identifier_no,
-        id_type = source.id_type,
-        customer_type = source.customer_type,
-        golden_id = source.golden_id,
-        product_name = source.product_name,
-        product_name_desc = source.product_name_desc,
-        product_status = source.product_status,
-        product_line = source.product_line,
-        product_group = source.product_group,
-        ban = source.ban,
-        billing_sub_no = source.billing_sub_no,
-        start_date = source.start_date,
-        end_date = source.end_date,
-        language = source.language,
-        street = source.street,
-        house_number = source.house_number,
-        moo = source.moo,
-        postal_code = source.postal_code,
-        city = source.city,
-        district = source.district,
-        sub_district = source.sub_district,
-        country = source.country,
-        first_name = source.first_name,
-        last_name = source.last_name,
-        ou_id = source.ou_id,
-        vip = source.vip,
-        company_code = source.company_code,
-        subscriber_code = source.subscriber_code,
-        subscriber_description = source.subscriber_description,
-        subscriber_type = source.subscriber_type,
-        subscriber_price_plan_code = source.subscriber_price_plan_code,
-        subscriber_status = source.subscriber_status,
-        subscriber_start_date = source.subscriber_start_date,
-        subscriber_end_date = source.subscriber_end_date,
-        update_at = NOW(),
-        transaction_date = source.transaction_date,
-        partition_date = source.partition_date,
-        partition_month = source.partition_month,
-        processed_date = source.processed_date
-WHEN NOT MATCHED THEN
-    INSERT (
-        grading_account_id,
-        identifier_no,
-        id_type,
-        customer_type,
-        customer_no,
-        crm_integration_id,
-        golden_id,
-        product_id,
-        product_name,
-        product_name_desc,
-        product_status,
-        product_line,
-        product_group,
-        ban,
-        billing_sub_no,
-        start_date,
-        end_date,
-        language,
-        street,
-        house_number,
-        moo,
-        postal_code,
-        city,
-        district,
-        sub_district,
-        country,
-        first_name,
-        last_name,
-        ou_id,
-        vip,
-        company_code,
-        subscriber_code,
-        subscriber_description,
-        subscriber_type,
-        subscriber_price_plan_code,
-        subscriber_status,
-        subscriber_start_date,
-        subscriber_end_date,
-        operator_name,
-        create_at,
-        update_at,
-        transaction_date,
-        partition_date,
-        partition_month,
-        processed_date
-    )
-    VALUES (
-        source.grading_account_id,
-        source.identifier_no,
-        source.id_type,
-        source.customer_type,
-        source.customer_no,
-        source.crm_integration_id,
-        source.golden_id,
-        source.product_id,
-        source.product_name,
-        source.product_name_desc,
-        source.product_status,
-        source.product_line,
-        source.product_group,
-        source.ban,
-        source.billing_sub_no,
-        source.start_date,
-        source.end_date,
-        source.language,
-        source.street,
-        source.house_number,
-        source.moo,
-        source.postal_code,
-        source.city,
-        source.district,
-        source.sub_district,
-        source.country,
-        source.first_name,
-        source.last_name,
-        source.ou_id,
-        source.vip,
-        source.company_code,
-        source.subscriber_code,
-        source.subscriber_description,
-        source.subscriber_type,
-        source.subscriber_price_plan_code,
-        source.subscriber_status,
-        source.subscriber_start_date,
-        source.subscriber_end_date,
-        source.operator_name,
-        NOW(),
-        NOW(),
-        source.transaction_date,
-        source.partition_date,
-        source.partition_month,
-        source.processed_date
-    );
-"""
+# transform_active_to_single_view = """
+# MERGE INTO {{ grading_schema }}.{{ single_view_subscriber_table }} AS target
+# USING (
+#     SELECT * 
+#     FROM {{ grading_schema }}.{{ single_staging_table }}
+#     WHERE partition_date = '{{ partition_date }}'
+#         AND operator_name = 'DTAC'
+#         AND product_status = 'ACTIVE'
+# ) AS source
+# ON target.product_id = source.product_id
+# AND target.customer_no = source.customer_no
+# AND target.operator_name = source.operator_name
+# WHEN MATCHED THEN 
+#     UPDATE SET 
+#         grading_account_id = source.grading_account_id,
+#         identifier_no = source.identifier_no,
+#         id_type = source.id_type,
+#         customer_type = source.customer_type,
+#         golden_id = source.golden_id,
+#         product_name = source.product_name,
+#         product_name_desc = source.product_name_desc,
+#         product_status = source.product_status,
+#         product_line = source.product_line,
+#         product_group = source.product_group,
+#         ban = source.ban,
+#         billing_sub_no = source.billing_sub_no,
+#         start_date = source.start_date,
+#         end_date = source.end_date,
+#         language = source.language,
+#         street = source.street,
+#         house_number = source.house_number,
+#         moo = source.moo,
+#         postal_code = source.postal_code,
+#         city = source.city,
+#         district = source.district,
+#         sub_district = source.sub_district,
+#         country = source.country,
+#         first_name = source.first_name,
+#         last_name = source.last_name,
+#         ou_id = source.ou_id,
+#         vip = source.vip,
+#         company_code = source.company_code,
+#         subscriber_code = source.subscriber_code,
+#         subscriber_description = source.subscriber_description,
+#         subscriber_type = source.subscriber_type,
+#         subscriber_price_plan_code = source.subscriber_price_plan_code,
+#         subscriber_status = source.subscriber_status,
+#         subscriber_start_date = source.subscriber_start_date,
+#         subscriber_end_date = source.subscriber_end_date,
+#         update_at = NOW(),
+#         transaction_date = source.transaction_date,
+#         partition_date = source.partition_date,
+#         partition_month = source.partition_month,
+#         processed_date = source.processed_date
+# WHEN NOT MATCHED THEN
+#     INSERT (
+#         grading_account_id,
+#         identifier_no,
+#         id_type,
+#         customer_type,
+#         customer_no,
+#         crm_integration_id,
+#         golden_id,
+#         product_id,
+#         product_name,
+#         product_name_desc,
+#         product_status,
+#         product_line,
+#         product_group,
+#         ban,
+#         billing_sub_no,
+#         start_date,
+#         end_date,
+#         language,
+#         street,
+#         house_number,
+#         moo,
+#         postal_code,
+#         city,
+#         district,
+#         sub_district,
+#         country,
+#         first_name,
+#         last_name,
+#         ou_id,
+#         vip,
+#         company_code,
+#         subscriber_code,
+#         subscriber_description,
+#         subscriber_type,
+#         subscriber_price_plan_code,
+#         subscriber_status,
+#         subscriber_start_date,
+#         subscriber_end_date,
+#         operator_name,
+#         create_at,
+#         update_at,
+#         transaction_date,
+#         partition_date,
+#         partition_month,
+#         processed_date
+#     )
+#     VALUES (
+#         source.grading_account_id,
+#         source.identifier_no,
+#         source.id_type,
+#         source.customer_type,
+#         source.customer_no,
+#         source.crm_integration_id,
+#         source.golden_id,
+#         source.product_id,
+#         source.product_name,
+#         source.product_name_desc,
+#         source.product_status,
+#         source.product_line,
+#         source.product_group,
+#         source.ban,
+#         source.billing_sub_no,
+#         source.start_date,
+#         source.end_date,
+#         source.language,
+#         source.street,
+#         source.house_number,
+#         source.moo,
+#         source.postal_code,
+#         source.city,
+#         source.district,
+#         source.sub_district,
+#         source.country,
+#         source.first_name,
+#         source.last_name,
+#         source.ou_id,
+#         source.vip,
+#         source.company_code,
+#         source.subscriber_code,
+#         source.subscriber_description,
+#         source.subscriber_type,
+#         source.subscriber_price_plan_code,
+#         source.subscriber_status,
+#         source.subscriber_start_date,
+#         source.subscriber_end_date,
+#         source.operator_name,
+#         NOW(),
+#         NOW(),
+#         source.transaction_date,
+#         source.partition_date,
+#         source.partition_month,
+#         source.processed_date
+#     );
+# """
